eae26b31e8195700907f80ff46a0d09750748080
killime <70292153+killime-moon@users.noreply.github.com>
2025-10-05 22:40:20 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+import certifi  # <- on ajoute certifi pour gérer le SSL
 
 load_dotenv()
-client = MongoClient(os.getenv("MONGO_URI"))
+
+# On force l'utilisation du certificat CA fourni par certifi
+client = MongoClient(os.getenv("MONGO_URI"), tlsCAFile=certifi.where())
+
 db = client["knk_db"]
 users = db["users"]
-
