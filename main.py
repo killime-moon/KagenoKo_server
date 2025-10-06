4e91ce93c8a60bc6237a70f53170f14977bf1a77
killime <70292153+killime-moon@users.noreply.github.com>
2025-10-06 22:44:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from routes import auth, quota
+from routes import auth_google_web
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/api/auth")
 app.include_router(quota.router, prefix="/api/quota")
+app.include_router(auth_google_web.router, prefix="/api/auth/google")
 
 @app.get("/")
 def home():
     return {"message": "Unity AI Server is running!"}
+
