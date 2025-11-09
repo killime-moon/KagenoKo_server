9344cf0b81454a136805423231f7c91918591dbe
killime <70292153+killime-moon@users.noreply.github.com>
2025-11-09 13:28:48 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
-def create_user(patreon_id: str, email: str):
+def create_user(patreon_id: str, email: str, tier_name: str):
     return {
         "patreon_id": patreon_id,
         "email": email,
+        "tier_name": tier_name,
         "quota": 50,
         "last_reset": datetime.utcnow().isoformat()
     }
+
 
